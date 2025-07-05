f3957e637546f33b58f407fa932f0f021841c1af
sharanteja2211 <sharanteja2211@gmail.com>
2025-07-05 17:10:43 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
-cd
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => res.send('API running 🚀'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
