a6424bd830b0f5c0512740d1dd6bfdc1d1b79f59
sharanteja2211 <sharanteja2211@gmail.com>
2025-07-05 17:40:41 +0530

    layout created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+// src/App.jsx
+import {Routes, Route} from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/Whishlist";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="wishlist" element={<Wishlist />} />
+        <Route path="product/:id" element={<ProductPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
